e124ecabdff79eb944c7177e3a9d9d47a089e998
EC2 Default User <ec2-user@ip-172-31-33-104.ec2.internal>
2023-06-04 05:36:10 +0000

    Added Files

---
+++ app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from aws_codepipeline.aws_codepipeline_stack import AwsCodepipelineStack
+
+
+app = cdk.App()
+AwsCodepipelineStack(app, "AwsCodepipelineStack",
+  
+
+    env=cdk.Environment(account='748389823986', region='us-east-1'),
+    stack_name='github-codepipeline-stack'
+
+    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+    )
+cdk.Tags.of(app).add(key='feature',value='resource_stack')
+cdk.Tags.of(app).add(key='contact',value='abc@againabc.com')
+cdk.Tags.of(app).add(key='team',value='best_team')
+app.synth()
